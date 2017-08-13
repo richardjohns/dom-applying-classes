afdf56615a861613f66c7956323c96bb10043269
richardjohns <you@example.com>
2017-08-13 15:17:12 +1200

    add yellow class & js to div 3

--- classes.js
+++ classes.js
@@ -7,6 +7,7 @@ function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
   one()
   two()
+  three()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -28,5 +29,11 @@ function two () {
 }
 
 // CREATE FUNCTION three HERE
+function three () {
+  // First, we have to find the element:
+  var three = document.getElementById('three')
 
+  // Next, we apply a new CSS class to it:
+  three.classList.toggle('yellow')
+}
 // CREATE FUNCTION makeVisible HERE
